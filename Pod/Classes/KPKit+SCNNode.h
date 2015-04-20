e5a07d151ed3d8a5cae78125831a19838f23624e
kitschpatrol <ermika@gmail.com>
2015-04-19 20:15:54 -0400

    Scenekit helpers.

---
+++ Pod/Classes/KPKit+SCNNode.h
@@ -0,0 +1,20 @@
+//
+//  KPKit+SCNNode.h
+//  Pods
+//
+//  Created by Eric Mika on 2/26/15.
+//
+//
+
+#import <GLKit/GLKit.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <SceneKit/SceneKit.h>
+
+@interface SCNNode (KPKit)
+
+//@property (nonatomic) GLKMatrix4 kp_glkTransform;
+@property (nonatomic) GLKVector3 kp_glkPosition;
+//@property (nonatomic) GLKVector4 kp_glkRotation;
+// TODO... moar?
+
+@end
---
+++ Pod/Classes/KPKit+SCNNode.m
@@ -0,0 +1,16 @@
+//
+//  KPKit+SCNNode.m
+//  Pods
+//
+//  Created by Eric Mika on 2/26/15.
+//
+//
+
+#import "KPKit+SCNNode.h"
+
+@implementation SCNNode (KPKit)
+
+- (GLKVector3)kp_glkPosition { return SCNVector3ToGLKVector3(self.position); }
+- (void)setKp_glkPosition:(GLKVector3)kp_glkPosition { self.position = SCNVector3FromGLKVector3(kp_glkPosition); }
+
+@end
